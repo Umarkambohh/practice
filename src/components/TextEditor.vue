4caf10f8c8ed05335c56458c8717e3ca2c6fb0b1
umar <umarsaleem512452@gmail.com>
2025-02-02 00:27:35 +0500

    add vueQuill

---
+++ src/components/TextEditor.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
+import { ref } from 'vue';
+
+const props = defineProps({
+  modelValue: {
+    type: String,
+  }
+});
+</script>
+
+<template>
+  <div id="my-toolbar">
+    <button>sds</button>
+    <button class="ql-bold"></button>
+    <button class="ql-italic"></button>
+  </div>
+  <QuillEditor v-model:content="props.modelValue" contentType="html" toolbar="#my-toolbar"></QuillEditor>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import { createPinia } from 'pinia';
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css';
 
 const pinia = createPinia()
 const app = createApp(App)
+app.component('QuillEditor', QuillEditor)
 
 app.use(pinia)
 app.mount('#app')
